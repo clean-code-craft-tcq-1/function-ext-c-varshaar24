4cc7f2ae20ed971c3f0b15b04511882c12e03e16
varshaar24 <77425787+varshaar24@users.noreply.github.com>
2021-03-07 18:21:53 +0530

    Update BMS_CheckBattery.c

--- BMS_CheckBattery.c
+++ BMS_CheckBattery.c
@@ -1,7 +1,6 @@
 #include <assert.h>
 #include <stdio.h>
 #include <stdbool.h>
-#include "BMS_Types.h"
 #include "BMS_Display.h"
 
 static int batteryIsOk(float temperature, float soc, float chargeRate)
@@ -12,22 +11,15 @@ static int batteryIsOk(float temperature, float soc, float chargeRate)
     bool Rateerr_b;
     int Ret;
 
-    if(0==CheckTemperatureInRange(temperature,(float)TEMPERATURE_UL,(float)TEMPERATURE_LL))
-    {
-        return 0;
-    }
-    else if(0==CheckSocInRange(soc,(float)SOC_UL,(float)SOC_LL))
-    {
-        return 0;
-    }
-    else if(0==CheckChargeRateInRange(chargeRate,(float)CHARGERATE_UL))
-    {
-        return 0;
-    }
-    else
+    Temperr_b = !CheckTemperatureInRange(temperature,(float)TEMPERATURE_UL,(float)TEMPERATURE_LL);
+    Socerr_b= !CheckSocInRange(soc,(float)SOC_UL,(float)SOC_LL);
+    Rateerr_b = !CheckChargeRateInRange(chargeRate,(float)CHARGERATE_UL);
+
+    Ret = ((Temperr_b | Socerr_b | Rateerr_b) != 0) ? 0 : 1;
+
+    if(Ret == 1)
     {
         DisplayBatteryFineStatus();
-        return 1;
     }
 
   return Ret;
@@ -39,10 +31,10 @@ int main()
     while(SelectLanguage());
 
     assert(batteryIsOk(25, 70, 0.7));
-   assert(!batteryIsOk(-1, 80, 0));
-   assert(!batteryIsOk(50, 85, 0));
-   assert(!batteryIsOk(25, 15, 0));
-   assert(!batteryIsOk(40, 85, 0));
-   assert(!batteryIsOk(40, 80, 0.9));
+    assert(!batteryIsOk(-1, 80, 0));
+    assert(!batteryIsOk(50, 85, 0));
+    assert(!batteryIsOk(25, 15, 0));
+    assert(!batteryIsOk(40, 85, 0));
+    assert(!batteryIsOk(40, 80, 0.9));
   return 0;
 }
