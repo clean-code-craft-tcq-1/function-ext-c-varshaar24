47c6e6ece6e2c20ccecd147b58f07065eb69aa52
varshaar24 <77425787+varshaar24@users.noreply.github.com>
2021-03-07 17:36:36 +0530

    Update BMS_CheckBattery.c

--- BMS_CheckBattery.c
+++ BMS_CheckBattery.c
@@ -29,16 +29,6 @@ static int batteryIsOk(float temperature, float soc, float chargeRate)
         DisplayBatteryFineStatus();
         return 1;
     }
-//    Temperr_b = !CheckTemperatureInRange(temperature,(float)TEMPERATURE_UL,(float)TEMPERATURE_LL);
-//    Socerr_b= !CheckSocInRange(soc,(float)SOC_UL,(float)SOC_LL);
-//    Rateerr_b = !CheckChargeRateInRange(chargeRate,(float)CHARGERATE_UL);
-//
-//    Ret = ((Temperr_b | Socerr_b | Rateerr_b) != 0) ? 0 : 1;
-//
-//    if(Ret == 1)
-//    {
-//        DisplayBatteryFineStatus();
-//    }
 
   return Ret;
 }
@@ -49,10 +39,10 @@ int main()
     while(SelectLanguage());
 
     assert(batteryIsOk(25, 70, 0.7));
-//    assert(!batteryIsOk(-1, 80, 0));
-//    assert(!batteryIsOk(50, 85, 0));
-//    assert(!batteryIsOk(25, 15, 0));
-//    assert(!batteryIsOk(40, 85, 0));
-//    assert(!batteryIsOk(40, 80, 0.9));
+   assert(!batteryIsOk(-1, 80, 0));
+   assert(!batteryIsOk(50, 85, 0));
+   assert(!batteryIsOk(25, 15, 0));
+   assert(!batteryIsOk(40, 85, 0));
+   assert(!batteryIsOk(40, 80, 0.9));
   return 0;
 }
